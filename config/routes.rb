540b380f61049d18821971f71de70f93a1812a74
yusei1224 <tech18izawa@gmail.com>
2016-09-26 19:02:01 +0900

    Use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   root 'prototypes#index'
-
-  get  '/prototypes/index'  =>    'prototypes#index'
-  get  '/prototypes/new'    =>    'prototypes#new'
-  get  '/prototypes/show'   =>    'prototypes#show'
+  resources :prototypes :only[:index,:new,:show]
 end
