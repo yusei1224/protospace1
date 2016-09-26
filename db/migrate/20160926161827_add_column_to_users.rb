7cfcc8782500a6941d7d98339c66e38d61489e66
yusei1224 <tech18izawa@gmail.com>
2016-09-27 01:27:41 +0900

    Add columns to users and rake db:migrate

---
+++ db/migrate/20160926161827_add_column_to_users.rb
@@ -0,0 +1,9 @@
+class AddColumnToUsers < ActiveRecord::Migration
+  def change
+  	 add_column :users, :name, :string
+  	 add_column :users, :works, :text
+  	 add_column :users, :profile, :text
+  	 add_column :users, :member, :string
+  end
+end
+
